f434f0b32847f146d378699daf350202ba14cf8b
midand-hash <midovandik07@gmail.com>
2024-12-27 12:11:22 +0300

    Добавил user.py

---
+++ user.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where are you live? ")
+
+print(f"\"This is `{name}`\"")
+print(f"\"It is `{age}`\"")
+print(f"\"(s)he live in `{location}`\"")
